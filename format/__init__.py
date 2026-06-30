a5b4a5dc70012b6b44ef9e1737df6ef97ab4c6e4
System-Virus-XTZ <System-Virus-XTZ@outlook.com>
2026-06-30 16:36:43 +0800

    Add format/__init__.py

---
+++ format/__init__.py
@@ -0,0 +1,32 @@
+"""Format package - Response formatting."""
+from .openai.render_chat import (
+    BuildChatCompletion as build_chat_completion,
+    BuildChatCompletionWithToolCalls as build_chat_completion_with_tool_calls,
+    build_chat_stream_chunk,
+    build_chat_stream_delta_choice,
+    build_chat_stream_finish_choice,
+)
+from .openai.render_stream_events import (
+    build_responses_created_payload,
+    build_responses_output_item_added_payload,
+    build_responses_output_item_done_payload,
+    build_responses_text_delta_payload,
+    build_responses_reasoning_delta_payload,
+    build_responses_completed_payload,
+    build_responses_failed_payload,
+)
+
+__all__ = [
+    "build_chat_completion",
+    "build_chat_completion_with_tool_calls",
+    "build_chat_stream_chunk",
+    "build_chat_stream_delta_choice",
+    "build_chat_stream_finish_choice",
+    "build_responses_created_payload",
+    "build_responses_output_item_added_payload",
+    "build_responses_output_item_done_payload",
+    "build_responses_text_delta_payload",
+    "build_responses_reasoning_delta_payload",
+    "build_responses_completed_payload",
+    "build_responses_failed_payload",
+]
